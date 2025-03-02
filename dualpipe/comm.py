7723486ce7f4b8621f9eca09f5c9e68d4a48796f
Erickrus <hyinghao@hotmail.com>
2025-03-02 10:45:48 +0800

    Update comm.py

--- dualpipe/comm.py
+++ dualpipe/comm.py
@@ -3,10 +3,14 @@ from typing import List, Tuple
 import torch
 import torch.distributed as dist
 
+'''
+Logic: 
+    Uses PyTorch’s dist.P2POp for asynchronous point-to-point communication.
+'''
 
+## Predefined shapes and data type for communication tensors, set via set_p2p_tensor_shapes and set_p2p_tensor_dtype.
 TENSOR_SHAPES: List[Tuple[int]] = None
 TENSOR_DTYPE: torch.dtype = None
-
 
 def set_p2p_tensor_shapes(shapes: List[Tuple[int]]):
     global TENSOR_SHAPES
@@ -23,6 +27,9 @@ def build_from_tensor_shapes():
 
 
 def append_irecv(ops: List[dist.P2POp], src: int, group: dist.ProcessGroup) -> List[torch.Tensor]:
+    '''
+    Queues a receive operation for input/gradient tensors.
+    '''
     tensors = build_from_tensor_shapes()
     src = dist.distributed_c10d.get_global_rank(group, src)
     for tensor in tensors:
@@ -32,6 +39,9 @@ def append_irecv(ops: List[dist.P2POp], src: int, group: dist.ProcessGroup) -> L
 
 
 def append_isend(ops: List[dist.P2POp], tensors: List[torch.Tensor], dst: int, group: dist.ProcessGroup) -> None:
+    '''
+    Queues a send operation for output/input gradient tensors.
+    '''
     dst = dist.distributed_c10d.get_global_rank(group, dst)
     for tensor in tensors:
         if tensor is not None:
